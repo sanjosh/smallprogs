af447674828af344a62990d8008df3860e0ecca7
sandeep <sandeep@zensan.(none)>
2014-11-05 21:41:56 +0530

    Minor mods to Gauss Circle prob

--- math/GaussCircleProblem.cpp
+++ math/GaussCircleProblem.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 
 /* 
- * Formula for Gauss circle problem 
+ * Number of distinct pairs for which x^2 + y^2 <= n
+ * is same as Formula for Gauss circle problem 
  * First few sequences sequence A000328        
  * which is Number of points of norm <= n^2 in square lattice. 
  *  http://oeis.org/A000328
@@ -13,8 +14,7 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     int r = atoi(argv[1]);
-
-    int r_sq = r*r;
+    int n = r*r;
 
     int64_t sum = 0;
     
@@ -23,11 +23,15 @@ int main(int argc, char* argv[])
     int64_t term2 = 0;
     do
     {
-        term1 = r_sq/((index << 2) + 1);
-        term2 = r_sq/((index << 2) + 3);
+        term1 = n/((index << 2) + 1);
+        term2 = n/((index << 2) + 3);
+        if (!term1) 
+        {
+            break;
+        }
         sum += (term1 - term2);
         index ++;
-    } while (term1 > 0);
+    } while (1);
 
-    cout << "Number of ways to represent N=" << r << " as sum of squares is="  << 1 + 4*sum << ":loop ends=" << index << endl;
+    cout << "Number of ways to represent N=" << n << " as sum of squares is="  << 1 + 4*sum << ":loop ends=" << index << endl;
 }
