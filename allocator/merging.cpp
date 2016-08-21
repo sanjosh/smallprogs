42d9dce605ba2213cf3a8636ab41b4ac56ca2a68
zensan <sanjos100@gmail.com>
2016-08-21 17:04:24 +0530

    made freelsit persistent in rocksdb

--- allocator/merging.cpp
+++ allocator/merging.cpp
@@ -1,69 +1,149 @@
-#include <map>
 #include <cstdint>
 #include <cassert>
+#include <string>
 #include <iostream>
 
 #include <random> // distrib
 #include <algorithm> // random_shuffle
 #include <unistd.h> // getpid
 
+#include <rocksdb/db.h>
+#include <rocksdb/comparator.h>
+
+using rocksdb::Slice;
+using rocksdb::DB;
+using rocksdb::WriteOptions;
+using rocksdb::Options;
+using rocksdb::ReadOptions;
+
+typedef uint32_t BlkId; // 4k multiple
+
+struct IntegerComparator : public rocksdb::Comparator 
+{
+  virtual int Compare(const Slice& as, const Slice& bs) const override
+  {
+    BlkId a = *(BlkId*)as.data();
+    BlkId b = *(BlkId*)bs.data();
+
+    if (a < b) {
+      return -1;
+    } else if (a > b) { 
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  virtual const char* Name() const override
+  {
+    return "coalesce-int-cmp";
+  }
+
+  virtual void FindShortestSeparator(
+    std::string* start,
+    const Slice& limit) const override
+  {
+  }
+
+  virtual void FindShortSuccessor(std::string* key) const override
+  {
+  }
+
+};
+
 class MergingAlloc
 {
   public:
-  typedef uint32_t BlkId; // 4k multiple
 
   private:
-  std::map<BlkId, size_t> freeList;
+  DB* db;
   size_t maxFreeListSize{0};
+  size_t freeListSize{0};
 
   // inverted and derived map sorted by size
   // map will have many entries for same size
   std::multimap<size_t, BlkId> sizeSortedList;
 
+  std::string kDBPath = "/tmp/coalescingDB";
+
   public:
 
   int init(size_t numSegs)
   {
-    freeList.insert(std::make_pair(0, numSegs));
+    kDBPath.append(std::to_string(getpid()));
+
+    rocksdb::Options options;
+    options.create_if_missing = true;
+    options.error_if_exists = true;
+    options.comparator = new IntegerComparator();
+    auto s = DB::Open(options, kDBPath, &db);
+    assert(s.ok());
+
+    BlkId start = 0;
+    Slice key((const char*)&start, sizeof(start));
+    Slice value((const char*)&numSegs, sizeof(numSegs));
+
+    db->Put(WriteOptions(), key, value);
     sizeSortedList.insert(std::make_pair(numSegs, 0));
+
     return 0;
   }
 
   int recover()
   {
+    // TODO rebuild sizeSortedList from db
     return 0;
   }
 
   int alloc(size_t numSegs, BlkId& ret_blkid)
   {
-    auto iter = freeList.begin();
-    static const auto iterEnd = freeList.end();
+    int retcode = -1;
+
+    auto iter = db->NewIterator(ReadOptions());
+
     bool found = false;
+
+    BlkId blkIdToUse = 0;
+    size_t numSegsInBlk = 0;
 
     // TODO use sizeSortedList
     auto sizeIter = sizeSortedList.lower_bound(numSegs);
     if (sizeIter != sizeSortedList.end()) {
-      iter = freeList.find(sizeIter->second);
-      assert(iter != iterEnd);
+
+      blkIdToUse = sizeIter->second;
+      numSegsInBlk = sizeIter->first;
+
+      Slice key((const char*)&blkIdToUse, sizeof(BlkId));
+
+      iter->Seek(key);
+      assert(iter->Valid());
+
       found = true;
     }
 
     if (found)
     {
-      if (iter->second > numSegs)
+      if (numSegsInBlk > numSegs)
       {
-        BlkId id = iter->first + numSegs;
-        size_t sz = iter->second - numSegs;
+        BlkId id = blkIdToUse + numSegs;
+        size_t sz = numSegsInBlk - numSegs;
 
-        freeList.insert(std::make_pair(id, sz));
+        db->Put(WriteOptions(), 
+          Slice((const char*)&id, sizeof(id)), 
+          Slice((const char*)&sz, sizeof(sz)));
+
         sizeSortedList.insert(std::make_pair(sz, id));
       }
-      ret_blkid = iter->first;
-      eraseSizeSortedList(iter->second, iter->first);
-      freeList.erase(iter);
-      return 0;
+      ret_blkid = blkIdToUse;
+      eraseSizeSortedList(numSegsInBlk, ret_blkid);
+      db->Delete(WriteOptions(), 
+        Slice((const char*)&ret_blkid, sizeof(ret_blkid)));
+      retcode = 0;
     }
-    return -1;
+
+    delete iter;
+
+    return retcode;
   }
 
   void eraseSizeSortedList(size_t sz, BlkId blkid)
@@ -93,75 +173,115 @@ class MergingAlloc
     BlkId insert_blkid = blkid;
     size_t insert_numSegs = numSegs;
 
-    static const auto iterEnd = freeList.end();
-    decltype(freeList)::iterator prevIter = iterEnd;
-    decltype(freeList)::iterator nextIter = iterEnd;
+    std::pair<BlkId, size_t> prevTuple;
+    bool prevValid = false;
 
-    for (auto iter = freeList.begin(); iter != iterEnd; iter ++)
+    std::pair<BlkId, size_t> nextTuple;
+    bool nextValid = false;
+
+    auto iter = db->NewIterator(ReadOptions());
+    iter->Seek(Slice((const char*)&blkid, sizeof(blkid)));
+
+    if (iter->Valid())
     {
-      if (iter->first < blkid) {
-        prevIter = iter;
-      } else if (iter->first > blkid) {
-        nextIter = iter;
-        break;
+      BlkId curId = *(BlkId*)iter->key().data();
+      size_t curSeg = *(size_t*)iter->value().data();
+
+      if (curId > blkid) {
+        nextTuple = std::make_pair(curId, curSeg);
+        nextValid = true;
+      }
+
+      iter->Prev();
+
+      if (iter->Valid()) {
+
+        curId = *(BlkId*)iter->key().data();
+        curSeg = *(size_t*)iter->value().data();
+
+        if (curId < blkid) {
+          prevTuple = std::make_pair(curId, curSeg);
+          prevValid = true;
+        }
       }
     }
 
-    //std::cout << "freeblk=" << blkid << ":" << numSegs << std::endl;
+    //std::cout << "new freeblk=" << blkid << ":" << numSegs << std::endl;
 
-    if (prevIter != iterEnd) {
+    if (prevValid) {
 
-      assert (prevIter->first < insert_blkid);
-      //std::cout << "  prev=" << prevIter->first << ":" << prevIter->second << std::endl;
+      assert (prevTuple.first < insert_blkid);
+      //std::cout << "  prev=" << prevTuple.first << ":" << prevTuple.second << std::endl;
 
       // check coalescing opportunity
-      assert(prevIter->first + prevIter->second <= blkid);
+      assert(prevTuple.first + prevTuple.second <= blkid);
 
-      if (prevIter->first + prevIter->second == blkid)
+      if (prevTuple.first + prevTuple.second == blkid)
       {
         // merge before
-        insert_blkid = prevIter->first;
-        insert_numSegs += prevIter->second;
-        eraseSizeSortedList(prevIter->second, prevIter->first);
-        freeList.erase(prevIter);
+        insert_blkid = prevTuple.first;
+        insert_numSegs += prevTuple.second;
+        eraseSizeSortedList(prevTuple.second, prevTuple.first);
+        db->Delete(WriteOptions(), 
+          Slice((const char*)&prevTuple.first, sizeof(prevTuple.first)));
       }
     }
 
-    if (nextIter != iterEnd) {
+    if (nextValid) {
 
-      assert (nextIter->first > insert_blkid);
-      //std::cout << "  next=" << nextIter->first << ":" << nextIter->second << std::endl;
+      assert (nextTuple.first > insert_blkid);
+      //std::cout << "  next=" << nextTuple.first << ":" << nextTuple.second << std::endl;
 
-      assert(insert_blkid + insert_numSegs <= nextIter->first);
+      assert(insert_blkid + insert_numSegs <= nextTuple.first);
 
-      if (insert_blkid + insert_numSegs == nextIter->first)
+      if (insert_blkid + insert_numSegs == nextTuple.first)
       {
         // merge after
-        insert_numSegs += nextIter->second;
-        eraseSizeSortedList(nextIter->second, nextIter->first);
-        freeList.erase(nextIter);
+        insert_numSegs += nextTuple.second;
+        eraseSizeSortedList(nextTuple.second, nextTuple.first);
+        db->Delete(WriteOptions(), 
+          Slice((const char*)&nextTuple.first, sizeof(nextTuple.first)));
       }
     }
 
-    freeList.insert(std::make_pair(insert_blkid, insert_numSegs));
+    //std::cout << "ins freeblk=" << insert_blkid << ":" << insert_numSegs << std::endl;
+    db->Put(WriteOptions(), 
+      Slice((const char*)&insert_blkid, sizeof(insert_blkid)), 
+      Slice((const char*)&insert_numSegs, sizeof(insert_numSegs)));
     sizeSortedList.insert(std::make_pair(insert_numSegs, insert_blkid));
-    maxFreeListSize = std::max(maxFreeListSize, freeList.size());
+
+    //TODO use estimate num keys
+    //maxFreeListSize = std::max(maxFreeListSize, freeList.size());
 
     return 0;
   }
 
-  void print(std::ostream& os)
+  void print(std::ostream& os, int level)
   {
-    for (auto elem : freeList)
+    os << "*****Start MergingAlloc free list" << std::endl;
+
+    auto iter = db->NewIterator(ReadOptions());
+    for (iter->SeekToFirst(); iter->Valid(); iter->Next())
     {
-      os << "blk=" << elem.first << ":num=" << elem.second << std::endl;
+      if (level > 1) {
+        os 
+          << "blk=" << *(BlkId*)iter->key().data()
+          << ":num=" << *(size_t*)iter->value().data()
+          << std::endl;
+      }
     }
+
+    delete iter;
+
     for (auto elem : sizeSortedList)
     {
-      os << "num=" << elem.first << ":blk=" << elem.second << std::endl;
+      if (level > 1) {
+        os << "num=" << elem.first << ":blk=" << elem.second << std::endl;
+      } 
     }
-    std::cout << "max freelist size=" << maxFreeListSize << std::endl;
-    // about MaxSegs / (2 * MaxAllocSize)
+    os << "max freelist size=" << maxFreeListSize << std::endl;
+    os << "*****End MergingAlloc free list" << std::endl;
+    // maxFreeListSize is about MaxSegs / (2 * MaxAllocSize)
   }
 
 };
@@ -177,7 +297,7 @@ int main(int argc, char* argv[])
   MergingAlloc myalloc;
   myalloc.init(MaxSegs);
 
-  std::vector<std::pair<MergingAlloc::BlkId, size_t>> retainList;
+  std::vector<std::pair<BlkId, size_t>> retainList;
 
   std::mt19937 seedGen(getpid());
   std::uniform_int_distribution<decltype(MaxSegs)> sizeGen(1, std::min(size_t(10), MaxSegs/8));
@@ -185,7 +305,7 @@ int main(int argc, char* argv[])
   int ret = 0;
   while (!ret) {
     size_t reqSize = sizeGen(seedGen);
-    MergingAlloc::BlkId blkid;
+    BlkId blkid;
 
     ret = myalloc.alloc(reqSize, blkid);
     if (ret == 0) {
@@ -193,18 +313,27 @@ int main(int argc, char* argv[])
     }
   }
 
-  myalloc.print(std::cout);
+  /*
+  std::cout << "--retainList begin--" << std::endl;
+  for (auto elem : retainList)
+  {
+    std::cout << "num=" << elem.first << ":blk=" << elem.second << std::endl;
+  }
+  std::cout << "--retainList end--" << std::endl;
+  */
+
+
+  myalloc.print(std::cout, 1);
 
   std::random_shuffle(retainList.begin(), retainList.end());
 
   for (auto elem : retainList)
   {
-    MergingAlloc::BlkId blkid;
+    BlkId blkid;
     size_t numSegs;
 
     myalloc.free(elem.first, elem.second);
   }
 
-  std::cout << "printing free list" << std::endl;
-  myalloc.print(std::cout);
+  myalloc.print(std::cout, 2);
 }
