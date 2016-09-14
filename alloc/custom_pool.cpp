3d8cb88db4956eac3d3a9d27159b4f8eabe72fae
zensan <sanjos100@gmail.com>
2016-09-14 08:38:32 +0530

    Set "*commit=true" in alloc_mmap still not working - arena0 alloc returns from same region as arena 16 alloc

--- alloc/custom_pool.cpp
+++ alloc/custom_pool.cpp
@@ -10,38 +10,52 @@ void *alloc_mmap(void *new_addr, size_t size, size_t alignment,
   void *p = mmap(nullptr, size, PROT_READ | PROT_WRITE, 
       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0); 
   std::cout << "MMAP=" << p << ":arena=" << arena_ind << ":size=" << size << std::endl;
+  if (*zero) {
+    bzero(p, size);
+    *zero = false;
+  }
+  *commit = true;
   return p;
 }
 
 int main(int argc, char* argv[])
 {
-    size_t size;
-    size_t mib[3];
-    size_t hooks_len;
-    chunk_hooks_t old_hooks;
-    chunk_hooks_t new_hooks;
     int ret = 0;
 
     unsigned arena_number;
-    size_t sz = sizeof(arena_number);
+    {
+      chunk_hooks_t old_hooks;
+      chunk_hooks_t new_hooks;
+      size_t hooks_len;
 
-    ret = mallctl("arenas.extend", &arena_number, &sz, nullptr, 0);
-    std::cout << "ret=" << ret << ":arena=" << arena_number << std::endl;
+      size_t sz = sizeof(arena_number);
 
-    std::string arenaName = "arena." + std::to_string(arena_number) + ".chunk_hooks";
+      ret = mallctl("arenas.extend", &arena_number, &sz, nullptr, 0);
+      std::cout << "ret=" << ret << ":arena=" << arena_number << std::endl;
 
-    hooks_len = sizeof(old_hooks);
-    ret = mallctl(arenaName.c_str(), &old_hooks, &hooks_len, nullptr, 0);
-    std::cout << "ret=" << ret << std::endl;
-    
-    old_hooks.alloc = alloc_mmap;
+      std::string arenaName = "arena." + std::to_string(arena_number) + ".chunk_hooks";
 
-    ret = mallctl(arenaName.c_str(), nullptr, nullptr, &old_hooks, hooks_len);
-    std::cout << "ret=" << ret << std::endl;
+      hooks_len = sizeof(old_hooks);
+      ret = mallctl(arenaName.c_str(), &old_hooks, &hooks_len, nullptr, 0);
+      std::cout << "ret=" << ret << std::endl;
+      
+      new_hooks = old_hooks;
+      new_hooks.alloc = alloc_mmap;
+
+      ret = mallctl(arenaName.c_str(), nullptr, nullptr, &new_hooks, hooks_len);
+      std::cout << "ret=" << ret << std::endl;
+    }
+
 
     for (int i = 0; i < 5; i++)
     {
       void* p = mallocx(1024, MALLOCX_ARENA(arena_number));
-      std::cout << "malloc=" << (void*)p << std::endl;
+      std::cout << "mmap malloc=" << (void*)p << std::endl;
     }
+    for (int i = 0; i < 5; i++)
+    {
+      void* p2 = mallocx(1024, MALLOCX_ARENA(0));
+      std::cout << "norm malloc=" << (void*)p2 << std::endl;
+    }
+
 }
