b3da44f73bc0ee8375d62b2125b32db799afe466
zensan <sanjos100@gmail.com>
2016-09-18 09:05:28 +0530

    modified example to use several threads and random distribution

--- throttling/rocksdb_rate_limiter.cc
+++ throttling/rocksdb_rate_limiter.cc
@@ -10,45 +10,63 @@
 #include "util/rate_limiter.h"
 
 #include <chrono>
+#include <future>
+#include <random>
 
 using namespace rocksdb;
 
 typedef std::chrono::high_resolution_clock Clock;
 
+std::unique_ptr<RateLimiter> limiter;
+
+int64_t rate_bytes_per_sec = 1024 * 1024; // 1 MB per sec
+int64_t refill_period_us = 1000000;
+int32_t fairness = 100;
+int32_t totalThreads = 5;
+
+void serverWorker(int index, int numreq) {
+
+  std::mt19937 seedGen(getpid() + index);
+  std::uniform_int_distribution<int64_t> reqGen(1, rate_bytes_per_sec/totalThreads);
+
+  while (numreq > 0) {
+
+    int64_t req = reqGen(seedGen);
+
+    limiter->Request(req, Env::IO_HIGH);
+
+    Clock::time_point end;
+    end = Clock::now();
+
+    std::time_t ttp = std::chrono::system_clock::to_time_t(end);
+
+    std::cout << index << "," << req << "," << std::ctime(&ttp);
+
+    numreq --;
+  }
+}
+
 int main(int argc, char* argv[]) {
 
-  int64_t rate_byter_per_sec = 1024;
-  int64_t refill_period_us = 1000000;
-  int32_t fairness = 10;
-
-  int req = 1;
+  int64_t req = 1024;
 
   if (argc > 1) { 
     req = atoi(argv[1]);
   }
 
-  std::unique_ptr<RateLimiter> limiter(new rocksdb::GenericRateLimiter(
-        rate_byter_per_sec,
+  limiter = std::unique_ptr<rocksdb::RateLimiter>(new rocksdb::GenericRateLimiter(
+        rate_bytes_per_sec,
         refill_period_us,
         fairness));
 
-  limiter->Request(rate_byter_per_sec, Env::IO_HIGH);
-
-  // unused time
-  usleep(refill_period_us * 5);
-
-  while (req > 0) {
-    Clock::time_point begin;
-    begin = Clock::now();
-
-    limiter->Request(rate_byter_per_sec, Env::IO_HIGH);
-
-    Clock::time_point end;
-    end = Clock::now();
-    int64_t interval = std::chrono::duration_cast<std::chrono::microseconds>(
-      end - begin).count();
-
-    std::cout << "sleep for " << interval << std::endl;
-    req --;
+  std::vector<std::future<void>> futvec;
+  for (int i = 0; i < totalThreads; i++) {
+    auto fut = std::async(std::launch::async, serverWorker, i, req);
+    futvec.push_back(std::move(fut));
   }
+
+  for (auto& fut : futvec) {
+    fut.wait();
+  }
+
 }
