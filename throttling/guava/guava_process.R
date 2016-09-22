f3b11f4cf8f1a63bb18c8b7109e2255605338527
zensan <sanjos100@gmail.com>
2016-09-22 08:31:13 +0530

    Updated guava and rocksdb rate limiter graph

--- throttling/guava/guava_process.R
+++ throttling/guava/guava_process.R
@@ -4,7 +4,8 @@ require(reshape2)
 d <- read.csv('guava.csv')
 
 ggplot(d, aes(time)) + 
-  geom_line(aes(y = permit, colour="var0")) +
+  geom_line(aes(y = permit, colour="peak")) +
+  geom_smooth(aes(y = permit, colour="average")) +
   # annotate("text", x = 150000, y = 800, label = "only hi pri;1024 bytes/sec")
 
 ggsave(file='guava.png')
--- throttling/rocksdb/rocksdb_process.R
+++ throttling/rocksdb/rocksdb_process.R
@@ -8,6 +8,7 @@ dlong = melt(d, id='time_us')
 p <- ggplot(data = dlong, 
   aes(x=time_us, y=value, colour = variable)) + 
   geom_smooth() +
+  geom_line() +
   annotate("text", x = 150000, y = 800, label = "only hi pri;1024 bytes/sec")
 
 ggsave(file='rocksdb_rate_limit.png')
