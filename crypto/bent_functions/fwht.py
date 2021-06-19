13d827a2b2e55d3b80d99a6f178904e42c5c2502
sanjosh <sanjos100@gmail.com>
2021-06-19 15:14:54 +0530

    fwht

--- crypto/bent_functions/fwht.py
+++ crypto/bent_functions/fwht.py
@@ -1,48 +1,66 @@
 from scipy import signal
 from sympy import fwht
 from scipy.fft import fft, ifft
+import numpy
 
-import matplotlib.pyplot as plot
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
 import numpy as np
+import math
 
-# Sampling rate 1000 hz / second
-t = np.linspace(0, 1, 128, endpoint=True)
-#print(t.shape)
-x = signal.square(2 * np.pi * 5 * t)
+# Generate truth table for random Boolean function
+#
+percent_ones = 0.5
+num_boolean_var = 4
+num_combinations = pow(2, num_boolean_var)
+
+max_coeff = num_combinations + 4
+t = np.linspace(0, 1, num_combinations, endpoint=True)
+print(t.shape[0])
+#x = signal.square(2 * np.pi * 1 * t)
 #x = signal.unit_impulse(t.shape)
+
+truth_table = numpy.random.choice([0, 1], size=t.shape[0], p=[1 - percent_ones, percent_ones])
+
 # Plot the square wave signal
-plot.subplot(3,1,1)
-plot.plot(t, x)
-plot.title('Square wave - 5 Hz sampled at 1000 Hz /second')
-plot.xlabel('Time')
-plot.ylabel('Amplitude')
-plot.grid(True, which='both')
-plot.axhline(y=0, color='k')
-plot.ylim(-2, 2)
+plt.subplot(3,1,1)
+plt.plot(t, truth_table)
+plt.title('Boolean function ' + str(num_boolean_var) + ' variables ')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.grid(True, which='both')
+plt.axhline(y=0, color='k')
+#plt.ylim(-max_coeff, max_coeff)
 
-y = fwht(x)
-plot.subplot(3,1,2)
-plot.plot(t, y, color='red')
-plot.title('WHT coefficients')
-plot.xlabel('Sequency index')
-plot.ylabel('Amplitude')
-plot.grid(True, which='both')
-plot.axhline(y=0, color='k')
-plot.ylim(-2, 2)
+y = fwht(truth_table)
+plt.subplot(3,1,2)
+plt.plot(t, y, color='red')
+plt.title('WHT coefficients')
+plt.xlabel('Sequency index')
+plt.ylabel('Amplitude')
+plt.grid(True, which='both')
+plt.axhline(y=0, color='k')
+#plt.ylim(-max_coeff, max_coeff)
 
 
-y = fft(x)
-plot.subplot(3,1,3)
-plot.plot(t, y)
-plot.title('FFT coefficients')
-plot.xlabel('fft index')
-plot.ylabel('Amplitude')
-plot.grid(True, which='both')
-plot.axhline(y=0, color='k')
-plot.ylim(-2, 2)
 
-plot.show()
+y = fft(truth_table)
+plt.subplot(3,1,3)
+plt.plot(t, y)
+plt.title('FFT coefficients')
+plt.xlabel('fft index')
+plt.ylabel('Amplitude')
+plt.grid(True, which='both')
+plt.axhline(y=0, color='k')
+#plt.ylim(-max_coeff, max_coeff)
+
+plt.subplots_adjust(left=0.1,
+                    bottom=0.1,
+                    right=0.9,
+                    top=0.9,
+                    wspace=0.4,
+                    hspace=1)
+
+plt.show()
 
 
