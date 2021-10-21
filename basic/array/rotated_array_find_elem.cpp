8d4dc22e71472e800c91b1211985cd6b2894d419
sanjosh <sanjos100@gmail.com>
2021-10-21 17:40:59 +0530

    docker setup

--- basic/array/rotated_array_find_elem.cpp
+++ basic/array/rotated_array_find_elem.cpp
@@ -114,3 +114,35 @@ int main(int argc, char* argv[])
 }
 
 
+
+/*
+        int start = 0;
+        int end = len - 1;
+
+        while(start <= end) {
+            int mid = (start + end)/2;
+
+            if(target == nums[mid]) {
+                return mid;
+            }
+
+            if(nums[start] <= nums[mid]) { // left array is sorted
+                if(target >= nums[start] && target < nums[mid]) { // target lies between start and mid index
+                    end = mid-1;
+                } else {
+                    start = mid+1;
+                }
+            } else { // right array is sorted
+                if(target > nums[mid] && target <= nums[end]) { // target lies between mid and end index
+                    start = mid+1;
+                } else {
+                    end = mid-1;
+                }
+            }
+        }
+
+        return -1;
+    }
+https://www.callicoder.com/rotated-sorted-array-search/
+
+*/
